e380131a2ee2053e099f36ff2fdb2e0385869093
Pinki Kumari <160412282+pinkimandal44066@users.noreply.github.com>
2024-05-05 13:57:08 +0530

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from './componants/HomeFolder/HomePage'
+
+const App = () => {
+  return (
+    <div className=''>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
